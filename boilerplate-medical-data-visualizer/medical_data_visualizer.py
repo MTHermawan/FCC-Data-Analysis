2ef15e5495914b83bea22a63cc8ff7903a05830e
Trigusni <109473523+MTHermawan@users.noreply.github.com>
2024-12-04 17:40:08 +0800

    Page View Time Series Visualizer

--- boilerplate-medical-data-visualizer/medical_data_visualizer.py
+++ boilerplate-medical-data-visualizer/medical_data_visualizer.py
@@ -25,6 +25,7 @@ def draw_cat_plot():
 
     # 6
     df_cat = df_cat.groupby(['variable', 'value', 'cardio'], as_index=False).size().rename(columns={'size': 'total'})
+    print(df_cat)
     
 
     # 7
